432581efc595fffbc11d1b68a0024d5895a8087f
Aleksandr Kasatkin <alexkasatkin90@gmail.ru>
2020-11-09 16:23:03 +0300

    Created a connection to the local DB

---
+++ main.py
@@ -0,0 +1,21 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+connection = create_connection("D:\\sm_app.sqlite")
+
+
+
+
+# TODO: CREATE BOTH TABLES
