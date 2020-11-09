1d0881d91260c1aea37bba610efeba05493fca83
Aleksandr Kasatkin <alexkasatkin90@gmail.ru>
2020-11-09 21:25:54 +0300

    Created a table + write/read sql template

--- main.py
+++ main.py
@@ -13,9 +13,55 @@ def create_connection(path):
     return connection
 
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
 connection = create_connection("D:\\sm_app.sqlite")
 
+# TODO: make a UI, asking for confirmation on every step
+create_users_table = """
+CREATE TABLE IF NOT EXISTS participants (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  education TEXT,
+  marital_status TEXT
+);
+"""
 
+execute_query(connection, create_users_table)
 
+# TODO: find a way to insert add variables to the insert queries
+create_users = """
+INSERT INTO
+  participants (name, age, gender, education, marital_status)
+VALUES
+  ('James', 25, 'male', 'MAI', 'single');
+"""
 
-# TODO: CREATE BOTH TABLES
+execute_query(connection, create_users)
+
+select_users = "SELECT * from participants"
+users = execute_read_query(connection, select_users)
+
+for user in users:
+    print(user)
